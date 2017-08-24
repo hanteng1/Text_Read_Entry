060c8f150d3e82dc040a00354d2f3c93cf4698c3
Teng Han <hanteng1021@outlook.com>
2017-08-24 20:28:21 +0900

    rough operation done

--- MarkingMenu/mobile/src/main/java/com/tenghan/markingmenu/DemoUIView.java
+++ MarkingMenu/mobile/src/main/java/com/tenghan/markingmenu/DemoUIView.java
@@ -43,10 +43,17 @@ public class DemoUIView extends View {
 
     public boolean isTriggered = false;
     public boolean isSubMenuing = false;
+    private boolean isSubMenuVertical = false;
+    private boolean isSubMenuLeftTop = false;
     private float subMenuWidth = 40;
+
+    private PointF subMenuTouchAchor = new PointF();
 
     private ArrayList<Integer> colorCode;
     private Random rand;
+    private int totalColor = 1000;
+    private int presentedColor = 100;
+    private int colorAnchor = 450;
 
 
     public DemoUIView(Context context, AttributeSet attrs) {
@@ -84,7 +91,7 @@ public class DemoUIView extends View {
         //generate color code
         colorCode = new ArrayList<Integer>();
         rand = new Random();
-        generateColor(100);
+        generateColor(totalColor);
 
         touchPoints = new ArrayList<PointF>();
 
@@ -109,7 +116,7 @@ public class DemoUIView extends View {
 
     private void generateColor(int num)
     {
-        int maxDistance = 20;
+        int maxDistance = 5;
 
         int a = 255;
         //set first to white
@@ -167,60 +174,67 @@ public class DemoUIView extends View {
 
         //set background color
 
-        /**
-         *          1
-         *          |
-         *          |
-         *    0 --------- 2
-         *          |
-         *          |
-         *          3
-         */
-        //for the commands tags
-        for(int itrc = 0; itrc < menuItems.length; itrc++)
+        if(!isSubMenuing)
         {
-            int posX = 1;
-            int posY = 1;
-            if(itrc == 0 || itrc == 2)
+            /**
+             *          1
+             *          |
+             *          |
+             *    0 --------- 2
+             *          |
+             *          |
+             *          3
+             */
+            //for the commands tags
+            for(int itrc = 0; itrc < menuItems.length; itrc++)
             {
-                posY = 0;
-                posX = 1;
-                if(itrc == 0)
+                int posX = 1;
+                int posY = 1;
+                if(itrc == 0 || itrc == 2)
                 {
-                    posX = -1;
+                    posY = 0;
+                    posX = 1;
+                    if(itrc == 0)
+                    {
+                        posX = -1;
+                    }
+                }else if(itrc == 1 || itrc == 3)
+                {
+                    posX = 0;
+                    posY = 1;
+                    if(itrc == 3)
+                    {
+                        posY = -1;
+                    }
                 }
-            }else if(itrc == 1 || itrc == 3)
-            {
-                posX = 0;
-                posY = 1;
-                if(itrc == 3)
-                {
-                    posY = -1;
-                }
-            }
-            canvas.drawText(menuItems[itrc], menuCenter.x + posX * menuDistance, menuCenter.y + posY * menuDistance, inputPaint);
-        }
-
-
-        //draw touch path
-        if (touchPoints.size() > 0)
-        {
-            touchPath.reset();
-            for (int itrt = 0; itrt < touchPoints.size(); itrt++)
-            {
-                if(itrt == 0)
-                {
-                    touchPath.moveTo(touchPoints.get(itrt).x, touchPoints.get(itrt).y);
-                }else
-                {
-                    touchPath.lineTo(touchPoints.get(itrt).x, touchPoints.get(itrt).y);
-                }
+                canvas.drawText(menuItems[itrc], menuCenter.x + posX * menuDistance, menuCenter.y + posY * menuDistance, inputPaint);
             }
 
-            canvas.drawPath(touchPath, strokePaint);
+
+            //draw touch path
+            if (touchPoints.size() > 0)
+            {
+                touchPath.reset();
+                for (int itrt = 0; itrt < touchPoints.size(); itrt++)
+                {
+                    if(itrt == 0)
+                    {
+                        touchPath.moveTo(touchPoints.get(itrt).x, touchPoints.get(itrt).y);
+                    }else
+                    {
+                        touchPath.lineTo(touchPoints.get(itrt).x, touchPoints.get(itrt).y);
+                    }
+                }
+
+                canvas.drawPath(touchPath, strokePaint);
+            }
+        }else
+        {
+            //based on which command is triggered
+
+            drawColorPanel(canvas, isSubMenuVertical, isSubMenuLeftTop);
         }
 
-        drawColorPanel(canvas, true, true);
     }
 
     public void onFingerDown(float x, float y)
@@ -237,6 +251,72 @@ public class DemoUIView extends View {
         if(isTriggered)
         {
             touchPoints.add(new PointF(x, y));
+
+            if(!isSubMenuing)
+            {
+                //gesture detection
+                int gestureResult = detectGesture(touchPoints);
+                switch (gestureResult)
+                {
+                    case -1:
+                        break;
+                    case 0:
+                        isSubMenuVertical = true;
+                        isSubMenuLeftTop = false;
+                        isSubMenuing = true;
+                        subMenuTouchAchor.set(x, y);
+                        break;
+                    case 1:
+                        isSubMenuVertical = false;
+                        isSubMenuLeftTop = false;
+                        isSubMenuing = true;
+                        subMenuTouchAchor.set(x, y);
+                        break;
+                    case 2:
+                        isSubMenuVertical = true;
+                        isSubMenuLeftTop = true;
+                        isSubMenuing = true;
+                        subMenuTouchAchor.set(x, y);
+                        break;
+                    case 3:
+                        isSubMenuVertical = false;
+                        isSubMenuLeftTop = true;
+                        isSubMenuing = true;
+                        subMenuTouchAchor.set(x, y);
+                        break;
+                    default:
+                        break;
+                }
+            }else
+            {
+                //submenu is activated
+                //operate on the submenu
+
+                if(isSubMenuVertical == true)
+                {
+                    //vertical, use y
+                    float diffY = y - subMenuTouchAchor.y;
+                    int diffColorAnchor = (int) (diffY * (totalColor - presentedColor)) / screenHeight;
+                    colorAnchor += diffColorAnchor;
+                }else
+                {
+                    //horizontal, use x
+                    float diffX = x - subMenuTouchAchor.x;
+                    int diffColorAnchor = (int) (diffX * (totalColor - presentedColor)) / screenWidth;
+                    colorAnchor += diffColorAnchor;
+
+                }
+
+                if( (colorAnchor + presentedColor)  > totalColor)
+                {
+                    colorAnchor = totalColor - presentedColor;
+                }else if(colorAnchor < 0)
+                {
+                    colorAnchor = 0;
+                }
+
+            }
+
             invalidate();
         }
 
@@ -245,6 +325,7 @@ public class DemoUIView extends View {
     public void onFingerUp(float x, float y)
     {
         isTriggered = false;
+        isSubMenuing = false;
         invalidate();
     }
 
@@ -255,6 +336,76 @@ public class DemoUIView extends View {
         invalidate();
     }
 
+
+    //gesture detection
+    private int detectGesture(ArrayList<PointF> points)
+    {
+
+        /**
+         *          1
+         *          |
+         *          |
+         *    0 --------- 2
+         *          |
+         *          |
+         *          3
+         */
+
+        int gestureResult = -1;
+
+        if(points.size() > 2)
+        {
+            float origX = points.get(0).x;
+            float origY = points.get(1).y;
+            float destX = points.get(points.size() - 1).x;
+            float destY = points.get(points.size() - 1).y;
+
+            if(calDistance(origX, origY, destX, destY) > menuDistance)
+            {
+                //a command is activated
+                float disX = destX - origX;
+                float disY = destY - origY;
+                if(Math.abs(disX) > Math.abs(disY))
+                {
+                    if(disX > 0)
+                    {
+                        //stroke to right
+                        gestureResult = 2;
+                    }else
+                    {
+                        //stroke to left
+                        gestureResult = 0;
+                    }
+                }else if(Math.abs(disX) < Math.abs(disY))
+                {
+                    if(disY < 0)
+                    {
+                        //stroke to top
+                        gestureResult = 1;
+                    }else
+                    {
+                        //stroke to bottom
+                        gestureResult = 3;
+                    }
+
+                }
+            }
+        }
+
+        return gestureResult;
+
+    }
+
+    private float calDistance(float x1, float y1, float x2, float y2)
+    {
+        return (float)Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+    }
+
+    //dont use this for now
+    private float calAngle(float x1, float y1, float x2, float y2)
+    {
+        return 0;
+    }
 
     private void drawColorPanel(Canvas canvas, boolean isVertical, boolean isleftTop)
     {
@@ -289,10 +440,10 @@ public class DemoUIView extends View {
             //divide the bars in vertical
             //0 - 1000, 10 * 10 * 10
 
-            float segment = mHeight / 100;
-            for(int itrc = 0; itrc < 100; itrc++)
+            float segment = mHeight / presentedColor;
+            for(int itrc = 0; itrc < presentedColor; itrc++)
             {
-                subMenuPaint.setColor(colorCode.get(itrc));
+                subMenuPaint.setColor(colorCode.get(colorAnchor + itrc));
                 canvas.drawRect(anchorX , anchorY + segment * itrc, anchorX + mWidth, anchorY + segment * itrc + segment, subMenuPaint);
             }
 
@@ -317,14 +468,20 @@ public class DemoUIView extends View {
                 //draw a slider
                 canvas.drawLine(anchorX + mWidth / 2, anchorY - mHeight, anchorX + mWidth / 2, anchorY + mHeight, subMenuSliderPaint);
             }
+
+
+            //draw color bars
+            //divide the bars in horizontal
+            //0 - 1000, 10 * 10 * 10
+
+            float segment = mWidth / presentedColor;
+            for(int itrc = 0; itrc < presentedColor; itrc++)
+            {
+                subMenuPaint.setColor(colorCode.get(colorAnchor + itrc));
+                canvas.drawRect(anchorX + segment * itrc , anchorY, anchorX + segment * itrc + segment, anchorY + mHeight, subMenuPaint);
+            }
+
         }
-
-
-
-
-
-        //canvas.drawRect(anchorX , anchorY, anchorX + mWidth, anchorY + mHeight, subMenuPaint);
-
     }
 
     private void drawFontSizePanel (Canvas canvas, float x, float y)
